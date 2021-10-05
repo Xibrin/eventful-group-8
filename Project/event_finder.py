5bdc1791ed37af760dc94fa817cde9f7ebe3ef0c
aryamanshodhan <ashodha1@jhu.edu>
2021-10-05 13:34:21 -0400

    Added basic implementation for ticketmaster API

--- Project/event_finder.py
+++ Project/event_finder.py
@@ -1,4 +1,15 @@
+import requests
+import os
+
 
 class EventFinder:
 
     def __init__(self):
+        pass
+
+    def events(self):
+        base_url = "https://app.ticketmaster.com/discovery/v2/events.json?&apikey="
+        key = os.getenv("TICKETMASTER_API_KEY")
+        response = requests.get(base_url + key)
+        print(response.json())
+        return response
--- Project/main.py
+++ Project/main.py
@@ -1,24 +1,16 @@
 # This is a sample Python script.
 import requests
 import os
-
-
-# TicketMaster API use for reference
-# def ticketmaster():
-#     base_url = "https://app.ticketmaster.com/discovery/v2/events.json?postalCode=21218&apikey="
-#     key = os.getenv("TICKETMASTER")
-#     response = requests.get(base_url + key)
-#     print(response.json())
+import event_finder
 
 class Travel:
-
     def __init__(self, start_pos, end_pos):
         self.start_pos = start_pos
         self.end_pos = end_pos
         self.route = None
 
     def directions(self, start_pos, end_pos):
-        base_url = "http://open.mapquestapi.com/directions/v2/route?key="
+        base_url = "https://open.mapquestapi.com/directions/v2/route?key="
         locations = "&from=" + start_pos + "&to=" + end_pos
         key = os.getenv("MAPQUEST_API_KEY")
         response = requests.get(base_url + key + locations)
@@ -27,21 +19,25 @@ class Travel:
         return response.json()
 
     def createRoute(self):
-        #use helper methods (or not depending on ease of implementation) to parse the directions into a route object
+        # use helper methods (or not depending on ease of implementation) to parse the directions into a route object
+        pass
 
-    #Helper Methods
+    # Helper Methods
     def get_fuel_cost(self):
-        #parse Json to get fuel cost
+        # parse Json to get fuel cost
+        pass
 
     def get_distance(self):
-        #parse Json to get distance
+        # parse Json to get distance
+        pass
 
     def get_time(self):
-        #parse Json to get time
-
+        # parse Json to get time
+        pass
 
 
 # Press the green button in the gutter to run the script.
-    if __name__ == '__main__':
-        directions("9E 33rd Baltimore, Maryland", "107 W 29th St, Baltimore, Maryland")
+if __name__ == '__main__':
+    travel = Travel("9E 33rd Baltimore, Maryland", "107 W 29th St, Baltimore, Maryland")
+    travel.directions("9E 33rd Baltimore, Maryland", "107 W 29th St, Baltimore, Maryland")
     # ticketmaster()

6f0af5f8bcd5c12db9aab959ef6b42e1b3757c2a
aryamanshodhan <ashodha1@jhu.edu>
2021-10-05 14:20:04 -0400

    Added database file

--- Project/main.py
+++ Project/main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 import requests
 import os
-import event_finder
+
 
 class Travel:
     def __init__(self, start_pos, end_pos):
@@ -9,16 +9,16 @@ class Travel:
         self.end_pos = end_pos
         self.route = None
 
-    def directions(self, start_pos, end_pos):
+    def directions(self):
         base_url = "https://open.mapquestapi.com/directions/v2/route?key="
-        locations = "&from=" + start_pos + "&to=" + end_pos
+        locations = "&from=" + self.start_pos + "&to=" + self.end_pos
         key = os.getenv("MAPQUEST_API_KEY")
         response = requests.get(base_url + key + locations)
-        print(response.json())
-        self.route = response.json()
-        return response.json()
+        route = response.json()
+        self.route = route
+        return route
 
-    def createRoute(self):
+    def create_route(self):
         # use helper methods (or not depending on ease of implementation) to parse the directions into a route object
         pass
 
@@ -36,8 +36,6 @@ class Travel:
         pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     travel = Travel("9E 33rd Baltimore, Maryland", "107 W 29th St, Baltimore, Maryland")
-    travel.directions("9E 33rd Baltimore, Maryland", "107 W 29th St, Baltimore, Maryland")
-    # ticketmaster()
+    travel.directions()

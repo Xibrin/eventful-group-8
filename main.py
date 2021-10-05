1bb986a9d20e29181d3c80fcb0538ab5f5a46659
AB Abdullah <ab_abdullah21@abs-macbook-air.acasmart.jh.edu>
2021-10-04 20:50:17 -0400

    added TicketMaster Base for future use

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import requests
 import os
 
 
+# TicketMaster API use for reference
+# def ticketmaster():
+#     base_url = "https://app.ticketmaster.com/discovery/v2/events.json?postalCode=21218&apikey="
+#     key = os.getenv("TICKETMASTER")
+#     response = requests.get(base_url + key)
+#     print(response.json())
+
+
 def directions(startPos, endPos):
     base_url = "http://open.mapquestapi.com/directions/v2/route?key="
     locations = "&from=" + startPos + "&to=" + endPos
@@ -14,3 +22,4 @@ def directions(startPos, endPos):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     directions("9E 33rd Baltimore, Maryland", "107 W 29th St, Baltimore, Maryland")
+    # ticketmaster()

41c9329779fc577bcb74f3f01c668d6508dcde80
aryamanshodhan <ashodha1@jhu.edu>
2021-10-04 21:32:56 -0400

    Testing to see if push works

---
+++ Project/main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+import requests
+import os
+
+
+# TicketMaster API use for reference
+# def ticketmaster():
+#     base_url = "https://app.ticketmaster.com/discovery/v2/events.json?postalCode=21218&apikey="
+#     key = os.getenv("TICKETMASTER")
+#     response = requests.get(base_url + key)
+#     print(response.json())
+
+
+def directions(startPos, endPos):
+    base_url = "http://open.mapquestapi.com/directions/v2/route?key="
+    locations = "&from=" + startPos + "&to=" + endPos
+    key = os.getenv("MAPQUEST_API_KEY")
+    response = requests.get(base_url + key + locations)
+    print(response.json())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    directions("9E 33rd Baltimore, Maryland", "107 W 29th St, Baltimore, Maryland")
+    # ticketmaster()

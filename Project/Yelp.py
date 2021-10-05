5f8cbb62b98d5c319d7311f62c0f9509334a7f87
Vasant-Bhardwaj-jhu <54721404+Vasant-Bhardwaj-jhu@users.noreply.github.com>
2021-10-05 14:21:55 -0400

    yelp update

--- Project/Yelp.py
+++ Project/Yelp.py
@@ -1,15 +1,17 @@
 import requests
 import json
 import os
+from datetime import datetime
+import calendar
 
 
 if __name__ == '__main__':
     headers = {'Authorization': 'Bearer %s' % os.getenv("YELP_API_KEY")}
 
-    url = 'https://api.yelp.com/v3/events/'
+    url = 'https://api.yelp.com/v3/events'
 
     # In the dictionary, term can take values like food, cafes or businesses like McDonalds
-    params = {'locale': 'cs_CZ'}
+    params = {'location': '9E 33rd Baltimore, Maryland', 'limit': 50, 'start_date': }
 
     # Making a get request to the API
     req = requests.get(url, params=params, headers=headers)
@@ -18,5 +20,8 @@ if __name__ == '__main__':
     print('The status code is {}'.format(req.status_code))
 
     # printing the text from the response
-    print(json.loads(req.text))
+    d = datetime.utcnow()
+    unixtime = calendar.timegm(d.utctimetuple())
+    print(unixtime)
+    #print(json.loads(req.text))
 

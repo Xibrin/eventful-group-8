4044672d33bfcfb07d0180324d38d01d53b93ed1
aryamanshodhan <ashodha1@jhu.edu>
2021-10-13 20:38:39 -0400

    Created new Django Project

--- eventPlanner/scheduler/apps.py
+++ eventPlanner/scheduler/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EventapplicationConfig(AppConfig):
+class SchedulerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eventapplication'
+    name = 'scheduler'

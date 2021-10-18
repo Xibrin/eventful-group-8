df3bf4efc97ab16723c89e6c03ba34101a6eb942
Peter Novello <peternovello@Peters-MacBook-Pro-2.local>
2021-10-17 22:34:41 -0400

    started adding Event models to model.py

--- eventPlanner/scheduler/models.py
+++ eventPlanner/scheduler/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length= 100)
+    id = models.IntegerField(primary_key=True, unique=True)
+    location = models.CharField(max_length=100)
+    start_time = models.CharField(max_length=10)
+    end_time = models.CharField(max_length=10)
+    cost = models.IntegerField()
+

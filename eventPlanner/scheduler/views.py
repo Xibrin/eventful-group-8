7a0928f476607e1dbb0d5b5e7ace9e2219fbed2a
Peter Novello <peternovello@Peters-MacBook-Pro-2.local>
2021-11-02 13:57:41 -0400

    commenting out irrelevant code

--- eventPlanner/scheduler/views.py
+++ eventPlanner/scheduler/views.py
@@ -101,7 +101,7 @@ def register_view(request):
                     nightlife = nightlife,
                     family = family
                 )
-                print(request.POST['row-1'])
+                #print(request.POST['row-1'])
                 current_user.save()
             except IntegrityError:
                 return render(request, "scheduler/register.html", context={
@@ -113,55 +113,55 @@ def register_view(request):
         return render(request, "scheduler/register.html", {
             "form": UserForm()
         })
-    if request.method == "POST":
-        first_name = request.POST["firstName"]
-        last_name = request.POST["lastName"]
-        email = request.POST["email"]
-        username = request.POST["email"]
-        password = request.POST["password"]
-        confirm_password = request.POST["confirmPassword"]
-        # music = request.POST["row-1"]
-        # visual  = request.POST["row-2"]
-        # performing =  request.POST["row-3"]
-        # film =  request.POST["row-4"]
-        # lectures  = request.POST["row-5"]
-        # fashion  = request.POST["row-6"]
-        # food =  request.POST["row-7"]
-        # festivals =  request.POST["row-8"]
-        # charity  = request.POST["row-9"]
-        # sports  = request.POST["row-10"]
-        # nightlife  = request.POST["row-11"]
-        # family  = request.POST["row-12"]
-
-
-        if password != confirm_password:
-            return render(request, "scheduler/register.html", context={
-                "invalidMessage": "Passwords do not match"
-            })
-
-        try:
-            current_user = User.objects.create_user(
-                first_name=first_name,
-                last_name=last_name,
-                username=email,
-                email=email,
-                password=password
-            )
-            print(request.POST['row-1'])
-            current_user.save()
-        except IntegrityError:
-            return render(request, "scheduler/register.html", context={
-                "invalidMessage": "Email address already in use"
-            })
-        login(request, current_user)
-        form = UserForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-
-        return render(request, "scheduler/login.html", context={
-            "successMessage": "Successfully created new user"
-        })
-    return render(request, "scheduler/register.html")
+    # if request.method == "POST":
+    #     first_name = request.POST["firstName"]
+    #     last_name = request.POST["lastName"]
+    #     email = request.POST["email"]
+    #     username = request.POST["email"]
+    #     password = request.POST["password"]
+    #     confirm_password = request.POST["confirmPassword"]
+    #     music = request.POST["row-1"]
+    #     visual  = request.POST["row-2"]
+    #     performing =  request.POST["row-3"]
+    #     film =  request.POST["row-4"]
+    #     lectures  = request.POST["row-5"]
+    #     fashion  = request.POST["row-6"]
+    #     food =  request.POST["row-7"]
+    #     festivals =  request.POST["row-8"]
+    #     charity  = request.POST["row-9"]
+    #     sports  = request.POST["row-10"]
+    #     nightlife  = request.POST["row-11"]
+    #     family  = request.POST["row-12"]
+    #
+    #
+    #     if password != confirm_password:
+    #         return render(request, "scheduler/register.html", context={
+    #             "invalidMessage": "Passwords do not match"
+    #         })
+    #
+    #     try:
+    #         current_user = User.objects.create_user(
+    #             first_name=first_name,
+    #             last_name=last_name,
+    #             username=email,
+    #             email=email,
+    #             password=password
+    #         )
+    #         print(request.POST['row-1'])
+    #         current_user.save()
+    #     except IntegrityError:
+    #         return render(request, "scheduler/register.html", context={
+    #             "invalidMessage": "Email address already in use"
+    #         })
+    #     login(request, current_user)
+    #     form = UserForm(request.POST or None)
+    #     if form.is_valid():
+    #         form.save()
+    #
+    #     return render(request, "scheduler/login.html", context={
+    #         "successMessage": "Successfully created new user"
+    #     })
+    # return render(request, "scheduler/register.html")
 
 
 @login_required
